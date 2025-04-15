702059ff630d322b8a451e26a71dc000a4b5ce3e
Devraj Joshi <devrajoshi444@gmail.com>
2025-04-15 23:19:23 +0545

    Refactor import statements to standardize casing for asyncHandler in authController and authMiddleware

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,7 +1,7 @@
 import User from "../models/User.js";
 import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
-import { asyncHandler } from "../utils/asyncHandler.js";
+import { asyncHandler } from "../utils/AsyncHandler.js";
 import jwt from "jsonwebtoken";
 
 // Register Controller
